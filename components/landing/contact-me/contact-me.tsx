fbe7fced97f117f24dc0525bca20d9f443683d15
Shivam Shahi <shivam.shahi06@gmail.com>
2024-02-24 12:36:19 +0530

    preparing for contact-dialog

--- components/landing/contact-me/contact-me.tsx
+++ components/landing/contact-me/contact-me.tsx
@@ -2,8 +2,7 @@ const ContactMe = () => {
     return ( 
         <main className="w-full flex flex-col gap-y-2 items-center justify-center px-4 md:px-36 lg:px-48 mt-12 scroll-mt-20" id="about-me">
             <div>
-                contact me form should be here 
-                lorem ipsum 
+                
             </div>
 
         </main>
