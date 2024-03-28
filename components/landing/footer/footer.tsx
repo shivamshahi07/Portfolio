9e23d5e71e10c34584d3bd241289dd4e5b1bcf63
Shivam Shahi <shivam.shahi06@gmail.com>
2024-03-28 11:39:58 +0530

    feat:feature responsive

--- components/landing/footer/footer.tsx
+++ components/landing/footer/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="w-screen">
           <hr />
         </div>
-        <div className="mb-1 mt-4 ml-8 mr-8 flex items-center justify-between">
+        <div className="mb-1 mt-4 ml-8 mr-8 flex-col sm:flex-row flex space-y-4 sm:space-y-0   items-center sm:justify-between">
           <div className="text-sm text-gray-500 tracking-wide sm:text-center dark:text-neutral-500">
             © 2024 Shivam Shahi.All rights reserved.
           </div>
