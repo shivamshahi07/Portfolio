3d7e54cf57d2552c90858b89f694b04b2c679eea
Shivam Shahi <shivam.shahi06@gmail.com>
2024-05-27 23:00:22 +0530

    new project

--- components/landing/projects/project-items.ts
+++ components/landing/projects/project-items.ts
@@ -1,6 +1,7 @@
 import img from "@/public/images/shlipkart.png";
 import portfolio from "@/public/images/portfolio.png";
 import web from "@/public/images/web.png";
+import pre from "@/public/images/pre.png";
 export const projectitems = [
   {
     id: 1,
@@ -56,12 +57,22 @@ export const projectitems = [
       "Shadcn UI",
     ],
   },
-  // {
-  //   id: 4,
-  //   name: "future-roject-name",
-  //   image: "future-image-link",
-  //   description: "future-project-description",
-  //   demolink: "future-demo-link",
-  //   github: "future-github-link",
-  // },
+  {
+    id: 4,
+    name: "Prototype YC landing page for Preloop",
+    image: pre,
+    description: "This website was my attempt to make a protoype marketing page for a recently funded(w24) YC startup, Preloop",
+    demolink: "https://preloop.vercel.app/",
+    github: "https://github.com/shivamshahi07/Preloop",
+    skills: [
+      "React",
+      "Next.js",
+      "MongoDB",
+      "Tailwind CSS",
+      "Framer Motion",
+      "Resend(Email)",
+      "Typescript",
+      "Shadcn UI",
+    ],
+  },
 ] as const;
